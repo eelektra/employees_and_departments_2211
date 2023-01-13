dbd02dbf4c8941ff68ffd94fd74c6fe31a1e5800
Carol Rieping <carol.rieping@gmail.com>
2023-01-13 09:15:24 -0700

    It1 has attributes

--- lib/employee.rb
+++ lib/employee.rb
@@ -1,8 +1,12 @@
 class Employee
 
+  attr :name,
+       :age,
+       :salary
+
   def initialize(info)
     @name = info[:name]
-    @age = info[:age]
-    @ssalary = info[:salary]
+    @age = info[:age].to_i
+    @salary = info[:salary].delete('$').to_i
   end
 end
--- spec/employee_spec.rb
+++ spec/employee_spec.rb
@@ -7,5 +7,13 @@ describe Employee do
       
       expect(bobbi).to be_a(Employee)
     end
+
+    it 'has attributes' do
+      bobbi = Employee.new({name: "Bobbi Jaeger", age: "30", salary: "$100000"})
+
+      expect(bobbi.name).to eq("Bobbi Jaeger")
+      expect(bobbi.age).to eq(30)
+      expect(bobbi.salary).to eq(100000)
+    end
   end
 end
